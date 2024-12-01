bb214ff9043793062728d189d49fa432bd490fc7
Marian <mariansusanaas@gmail.com>
2024-11-30 20:47:27 -0500

    feat: add startup.sh

--- client/app.py
+++ client/app.py
@@ -1,3 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('frontend.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
